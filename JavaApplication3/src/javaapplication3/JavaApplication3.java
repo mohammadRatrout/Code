e23ec185fca673dccc351838bac5522e5ca39da3
mohammadRatrout <mohammad.ratrout@gmail.com>
2017-02-27 19:35:49 +0200

    yes

---
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author Mohd
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        for(int i=0;i<300;i++)
+        {
+            for(int j=0;j<300;j++)
+            {
+                for(int z=0;z<100;z++)
+                {
+                    System.out.print(z);
+                }
+            }
+        }
+    }
+    
+}
